12921f2734664c552aa963dc78c2d393b2641767
锦之米焰 <3040054935@qq.com>
2024-03-14 20:07:22 +0800

    Menu basic switch Settings

---
+++ MenuManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance;
+
+    [SerializeField] Menu[] menus;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public void openMenu(string menuName)
+    {
+        for(int i = 0; i < menus.Length; i++)
+        {
+            if (menus[i].menuName == menuName)
+            {
+                openMenu(menus[i]); 
+            }
+            else if (menus[i].open)
+            {
+                closeMenu(menus[i]);
+            }
+        }
+    }
+    public void openMenu(Menu menu)
+    {
+        for (int i = 0; i < menus.Length; i++)
+        {
+            if (menus[i].open)
+            {
+                closeMenu(menus[i]);
+            }
+        }
+        menu.Open();
+    }
+    public void closeMenu(Menu menu)
+    {
+        menu.Close();
+    }
+}
